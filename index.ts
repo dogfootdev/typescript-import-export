50a627e09b62ecd56c9ad1ab96518da131a6fde6
dogfootdev <no10305@gmail.com>
2021-10-25 19:10:28 +0900

    1 file modified

---
+++ index.ts
@@ -0,0 +1,5 @@
+import * as total from './f';
+console.log('total.foo() : ' + total.foo());
+console.log('total.bar() : ' + total.bar());
+console.log('total.result : ' + total.result);
+total.meow(11);
